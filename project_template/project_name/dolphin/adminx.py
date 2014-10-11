eaebd25e65a72dd1930448e3c355b9e954f3fa78
Leonardo.Z <glasslion@gmail.com>
2014-10-11 16:19:05 +0800

    Enabel bootstrap theme for django xadmin

--- project_template/project_name/dolphin/adminx.py
+++ project_template/project_name/dolphin/adminx.py
@@ -4,3 +4,8 @@ from django.db.models import get_models, get_app
 
 for model in get_models(get_app('dolphin')):
     xadmin.site.register(model)
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+xadmin.site.register(xadmin.views.BaseAdminView, BaseSetting)
