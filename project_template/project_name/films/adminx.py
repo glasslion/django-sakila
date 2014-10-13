d269f101e562f5bc1480fa7fda3867054165a68d
Leonardo.Z <glasslion@gmail.com>
2014-10-13 10:45:56 +0800

    Add admin settings

--- project_template/project_name/films/adminx.py
+++ project_template/project_name/films/adminx.py
@@ -6,3 +6,8 @@ xadmin.site.register(Actor)
 xadmin.site.register(Category)
 xadmin.site.register(Film)
 xadmin.site.register(Language)
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+xadmin.site.register(xadmin.views.BaseAdminView, BaseSetting)
